de2637771f9af68952025139fea8d53a21d909f1
Gurpreet Brar <guribrar200@gmail.com>
2025-02-20 01:17:02 -0500

    update events.js to include route for getting event details

--- routes/events.js
+++ routes/events.js
@@ -3,11 +3,13 @@ import {
   getEvents,
   getFeaturedEvents,
   postEvent,
+  getEvent,
 } from "../controlllers/events-controller.js";
 
 const router = express.Router();
 
 router.route("/").get(getEvents).post(postEvent);
+router.route("/:id").get(getEvent);
 router.route("/featured").get(getFeaturedEvents);
 
 export default router;
